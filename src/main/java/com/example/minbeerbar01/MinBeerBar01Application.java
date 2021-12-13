d94b47e2ce4df207d3c6d23d912af4ad65070266
ERLMKea <erlm@kea.dk>
2021-12-13 17:26:37 +0100

    Startet nyt projekt. Med JT .gitignore fil

---
+++ src/main/java/com/example/minbeerbar01/MinBeerBar01Application.java
@@ -0,0 +1,13 @@
+package com.example.minbeerbar01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinBeerBar01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinBeerBar01Application.class, args);
+    }
+
+}
